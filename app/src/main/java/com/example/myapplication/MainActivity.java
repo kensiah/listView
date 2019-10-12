87b14fa1549ff03bd81a12ee7eb9808be6d13a1b
kensiah <siahkianann0479@e.newera.edu.my>
2019-10-12 11:25:40 +0800

    listview

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btnStartActivity;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnStartActivity = findViewById(R.id.btnStartActivity);
+
+    }
+
+    public void startActivity2(View v){
+        Intent i= new Intent(this, ListViewActivity.class);
+        startActivity(i);
+    }
+}
